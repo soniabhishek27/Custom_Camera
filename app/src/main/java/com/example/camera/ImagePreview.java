12abafa7c510e00054a05400cd8754d179d26295
Abhishek soni <abhisheksoni3216@gmail.com>
2019-08-28 13:14:34 +0530

    Initial commit

---
+++ app/src/main/java/com/example/camera/ImagePreview.java
@@ -0,0 +1,14 @@
+package com.example.camera;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ImagePreview extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_image_preview);
+    }
+}
